734837294906606c3579b32f23f60abec92fc682
Gerardo Guzman <jerryguzman873@gmail.com>
2016-11-16 15:41:44 -0800

    part 3

--- simplegame.js
+++ simplegame.js
@@ -10,3 +10,8 @@ document.addEventListener("keydown", function(e) {
  document.getElementById("perry").setAttribute("x", PerryX)
   }
 })
+
+var heart1X=Number(document.getElementById("heart1").getAttribute("x"));
+ var heart1y=Number(document.getElementById("heart1").getAttribute("y"));
+ var heart2X=Number(document.getElementById("heart2").getAttribute("x"));
+ var heart2y=Number(document.getElementById("heart2").getAttribute("y"));
